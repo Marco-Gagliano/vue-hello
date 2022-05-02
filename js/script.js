323c05d9e976487d615372c69adec78ec69e53fd
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-02 14:11:57 +0200

    scrittura-messaggio-da-vuejs-in-html

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
+
+
+const app = new Vue({
+  // da qui in poi si siscriverà tutto il codice
+
+  
+  el:'#app',
+  //"EL" el, che sta per elemento", è un parametro obbligato e viene indirizzato all'ID 
+
+
+  data: {
+    messaggio: 'Benvenuto in VueJs'
+  }
+  // tutte le varibiali che verrano inserite verrano dichiarate in "DATA"
+
+});
